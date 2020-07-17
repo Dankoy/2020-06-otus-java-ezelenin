432ce617cb11dd45d6a665ecd1d1c59c4c4a411d
Evgeny <transingularity@gmail.com>
2020-07-18 00:12:45 +0300

    refactor: Удалены мусор

--- hw02-generics/src/main/java/ru/dankoy/otus/DIYArrayListMain.java
+++ hw02-generics/src/main/java/ru/dankoy/otus/DIYArrayListMain.java
@@ -2,45 +2,47 @@ package ru.dankoy.otus;
 
 import ru.dankoy.otus.generics.DIYArrayList;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class DIYArrayListMain {
 
     public static void main(String[] args) {
 
-        List<Integer> integerList = new DIYArrayList<>();
-        List<Double> doubleList = new DIYArrayList<>();
+        List<Integer> integerList1 = new DIYArrayList<>();
+        List<Integer> integerList2 = new DIYArrayList<>();
+//        List<Double> doubleList = new DIYArrayList<>();
 
-//        populateArrayWithIntegers(integerList);
-        populateArrayWithDoubles(doubleList);
+        populateArrayWithIntegers(integerList1, 30);
+        populateArrayWithIntegers(integerList2, 30);
+//        populateArrayWithDoubles(doubleList);
 
+        printArray(integerList1);
+        printArray(integerList2);
+//        printArray(doubleList);
 
-//        printArray(integerList);
-        printArray(doubleList);
+//        Заменяет элементы первого листа вторым
+        Collections.copy(integerList1, integerList2);
+        printArray(integerList1);
 
-        Iterator listIterator = doubleList.listIterator();
-        int k = 0;
-        while (listIterator.hasNext()) {
-            System.out.println(k + ": "  + listIterator.next());
-            k++;
-        }
+        // Сортировка, работает с компаратором и без
+        Collections.sort(integerList1, Collections.reverseOrder());
+        printArray(integerList1);
 
-//        Collections.copy(integerList, integerList);
+        // Добавление элементов к листу
+        Collections.addAll(integerList1, 1,2,3,4,5);
+        printArray(integerList1);
 
     }
 
-    private static void populateArrayWithIntegers(List<Integer> integerList) {
+    public static void populateArrayWithIntegers(List<Integer> integerList, int size) {
 
-        for (int i = 0; i < integerList.size(); i++) {
+        for (int i = 0; i < size; i++) {
             integerList.add((int) (Math.random() * 1000));
         }
 
     }
 
-    private static void populateArrayWithDoubles(List<Double> doubleList) {
+    public static void populateArrayWithDoubles(List<Double> doubleList) {
 
         for (int i = 0; i < doubleList.size(); i++) {
             doubleList.add((Math.random() * 1000));
@@ -48,13 +50,15 @@ public class DIYArrayListMain {
 
     }
 
-    private static void printArray(List<?> arrayList) {
+    public static void printArray(List<?> arrayList) {
 
         System.out.println("Size: " + arrayList.size());
-        for (int i = 0; i < arrayList.size(); i++) {
-            System.out.println(arrayList.get(i));
+        Iterator listIterator = arrayList.listIterator();
+        int k = 0;
+        while (listIterator.hasNext()) {
+            System.out.println(k + ": "  + listIterator.next());
+            k++;
         }
-
     }
 
 }
