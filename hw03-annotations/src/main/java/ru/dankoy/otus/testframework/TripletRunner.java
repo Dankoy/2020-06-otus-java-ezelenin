fe97d7d3b6c6a4b36c70662f1c28f97a002f310f
Evgenii Zelenin <ezelenin@dtln.ru>
2020-07-24 17:54:57 +0300

    refactor: реализован метод прогона тестов after, test и before

--- hw03-annotations/src/main/java/ru/dankoy/otus/testframework/TripletRunner.java
+++ hw03-annotations/src/main/java/ru/dankoy/otus/testframework/TripletRunner.java
@@ -1,19 +1,20 @@
 package ru.dankoy.otus.testframework;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 
 /**
- * Класс описывает объект с полями
- * List<Method> before - список всех методов с аннотацией Before. Так как таких методов может
- * быть много и они все должны быть выполнены до выполнения теста - потому передается лист.
- * Method test - один метод с аннотацией @Test
- * List<Method> after - список методов с аннотацией @After. Так как таких методов может быть
- * много и все они должны быть выполнены после выполнения самого теста - поэтому передается лист
- * boolean status - статус завершения теста.
+ * Класс описывает объект с полями List<Method> before - список всех методов с аннотацией Before.
+ * Так как таких методов может быть много и они все должны быть выполнены до выполнения теста -
+ * потому передается лист. Method test - один метод с аннотацией @Test List<Method> after - список
+ * методов с аннотацией @After. Так как таких методов может быть много и все они должны быть
+ * выполнены после выполнения самого теста - поэтому передается лист boolean status - статус
+ * завершения теста. Class<?> clazz - Передаваемый отрефлексированный класс.
  */
 public class TripletRunner {
 
+  private Class<?> clazz;
   private List<Method> before;
   private Method test;
   private List<Method> after;
@@ -43,7 +44,7 @@ public class TripletRunner {
     this.after = after;
   }
 
-  public boolean isStatus() {
+  public boolean getStatus() {
     return status;
   }
 
@@ -65,16 +66,37 @@ public class TripletRunner {
 
   }
 
-  public TripletRunner(List<Method> before, Method test, List<Method> after) {
+  public TripletRunner(List<Method> before, Method test, List<Method> after, Class<?> clazz) {
     this.before = before;
     this.test = test;
     this.after = after;
+    this.clazz = clazz;
   }
 
-  public void runTest() {
-    //#TODO Дописать
+  public void runTest()
+      throws InvocationTargetException, IllegalAccessException {
 
-    this.status = false;
+    // Создание объекта тестирования
+    Object object = ReflectionHelper.instantiate(clazz);
+
+    // Run all before methods
+    for (Method beforeMethod : before) {
+      beforeMethod.invoke(object);
+    }
+
+    // Run test
+    try {
+      test.invoke(object);
+      status = true;
+    } catch (Exception e) {
+      status = false;
+    }
+
+    // Run all After methods
+    for (Method afterMethod : after) {
+      afterMethod.invoke(object);
+    }
+
   }
 
 }
