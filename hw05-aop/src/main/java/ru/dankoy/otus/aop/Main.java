e357063cbadfe1d736c62559e59dcc45f3d3e05c
Evgeny <transingularity@gmail.com>
2020-09-02 17:23:15 +0300

    refactor: Добавлен пример реализации AOP в Main класс

--- hw05-aop/src/main/java/ru/dankoy/otus/aop/Main.java
+++ hw05-aop/src/main/java/ru/dankoy/otus/aop/Main.java
@@ -1,4 +1,20 @@
 package ru.dankoy.otus.aop;
 
+import ru.dankoy.otus.aop.proxy.ClassForAop;
+import ru.dankoy.otus.aop.proxy.ClassForAopImpl;
+import ru.dankoy.otus.aop.proxy.DynamicInvocationHandler;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        DynamicInvocationHandler handler = new DynamicInvocationHandler(new ClassForAopImpl(), ClassForAop.class);
+        ClassForAop classForAopObject = (ClassForAop) handler.createClassForAop();
+        classForAopObject.calculation(5, 5, 76, 8, 9);
+        classForAopObject.calculation(0);
+        classForAopObject.calculation2(5, 5, 76, 8, 9);
+        classForAopObject.calculation2(0);
+
+    }
+
 }
