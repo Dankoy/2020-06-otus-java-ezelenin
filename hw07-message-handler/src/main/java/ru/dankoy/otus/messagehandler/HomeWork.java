938c6e45e2c311bbc4557c53b9883e1c3232ec07
Evgeny <transingularity@gmail.com>
2020-09-19 16:00:45 +0300

    refactor: В main класс добавлен листенер для сохранения истории изменений сообщений

--- hw07-message-handler/src/main/java/ru/dankoy/otus/messagehandler/HomeWork.java
+++ hw07-message-handler/src/main/java/ru/dankoy/otus/messagehandler/HomeWork.java
@@ -1,6 +1,7 @@
 package ru.dankoy.otus.messagehandler;
 
 import ru.dankoy.otus.messagehandler.handler.ComplexProcessor;
+import ru.dankoy.otus.messagehandler.listener.ListenerHistorySaver;
 import ru.dankoy.otus.messagehandler.listener.ListenerPrinter;
 import ru.dankoy.otus.messagehandler.processor.LoggerProcessor;
 import ru.dankoy.otus.messagehandler.processor.ProcessorConcatFields;
@@ -33,6 +34,9 @@ public class HomeWork {
         var listenerPrinter = new ListenerPrinter();
         complexProcessor.addListener(listenerPrinter);
 
+        var listenerHistorySaver = new ListenerHistorySaver();
+        complexProcessor.addListener(listenerHistorySaver);
+
         var message = new Message.Builder()
                 .field1("field1")
                 .field2("field2")
@@ -43,9 +47,23 @@ public class HomeWork {
                 .field13("field13")
                 .build();
 
+        var message2 = new Message.Builder()
+                .field2("field2")
+                .field3("field3")
+                .field6("field6")
+                .field10("field10")
+                .field11("field11")
+                .field13("field13")
+                .build();
+
         var result = complexProcessor.handle(message);
+        System.out.println("result:" + result);
+        var result2 = complexProcessor.handle(message2);
         System.out.println("result:" + result);
 
         complexProcessor.removeListener(listenerPrinter);
+
+        System.out.println(listenerHistorySaver.getHistory());
+
     }
 }
