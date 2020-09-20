3094a28129395e0793d7a321ae9123bd4393eacf
Evgeny <transingularity@gmail.com>
2020-09-20 17:29:01 +0300

    refactor: Обновлен Main класс

--- hw-08-serialization/src/main/java/ru/dankoy/otus/serialization/Main.java
+++ hw-08-serialization/src/main/java/ru/dankoy/otus/serialization/Main.java
@@ -1,10 +1,40 @@
 package ru.dankoy.otus.serialization;
 
+import ru.dankoy.otus.serialization.guineapig.GuineaPig;
+import ru.dankoy.otus.serialization.serializer.Serializer;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        GuineaPig guineaPig =
+                GuineaPig.newBuilder()
+                        .setAge(10)
+                        .setName("name")
+                        .setSomeList(List.of("str1", "str2"))
+                        .setSomeList2(List.of(4, 6))
+                        .setSomeMap(new HashMap<>())
+                        .setBool(true)
+                        .setBool2(false)
+                        .build();
 
+        GuineaPig guineaPig2 =
+                GuineaPig.newBuilder()
+                        .setAge(15)
+                        .setName("name2")
+                        .setSomeList(List.of("str123", "str243"))
+                        .setBool(true)
+                        .setBool2(false)
+                        .build();
+
+        Serializer serializer = new Serializer();
+
+        serializer.serializeObject(guineaPig);
+        serializer.serializeObject(guineaPig2);
 
     }
 
