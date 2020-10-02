e857d41aea96edf75d3a378f2adf902a7e235d70
Evgeny <transingularity@gmail.com>
2020-10-02 21:06:13 +0300

    refactor: Добавлена реализация метода insert для UserDao через Mapper

--- hw09-jdbc/src/main/java/ru/dankoy/otus/jdbc/dao/UserDaoJdbcMapper.java
+++ hw09-jdbc/src/main/java/ru/dankoy/otus/jdbc/dao/UserDaoJdbcMapper.java
@@ -1,34 +1,46 @@
 package ru.dankoy.otus.jdbc.dao;
 
 import ru.dankoy.otus.core.dao.UserDao;
+import ru.dankoy.otus.core.dao.UserDaoException;
 import ru.dankoy.otus.core.model.User;
 import ru.dankoy.otus.core.sessionmanager.SessionManager;
 import ru.dankoy.otus.jdbc.mapper.JdbcMapper;
+import ru.dankoy.otus.jdbc.sessionmanager.SessionManagerJdbc;
 
 import java.util.Optional;
 
 public class UserDaoJdbcMapper implements UserDao {
 
-    private SessionManager sessionManager;
-    private JdbcMapper<?> jdbcMapper;
+    private final SessionManagerJdbc sessionManager;
+    private final JdbcMapper<User> jdbcMapper;
 
-    public UserDaoJdbcMapper(SessionManager sessionManager, JdbcMapper<?> jdbcMapper) {
+    public UserDaoJdbcMapper(SessionManagerJdbc sessionManager, JdbcMapper<User> jdbcMapper) {
         this.sessionManager = sessionManager;
         this.jdbcMapper = jdbcMapper;
     }
 
     @Override
     public Optional<User> findById(long id) {
+
+//        jdbcMapper.findById(id);
+
         return Optional.empty();
     }
 
     @Override
     public long insertUser(User user) {
+
+        try {
+            jdbcMapper.insert(user);
+        } catch (Exception e) {
+            throw new UserDaoException(e);
+        }
+
         return 0;
     }
 
     @Override
     public SessionManager getSessionManager() {
-        return null;
+        return sessionManager;
     }
 }
