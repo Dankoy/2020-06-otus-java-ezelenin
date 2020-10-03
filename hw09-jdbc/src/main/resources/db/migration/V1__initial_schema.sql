0d490706995fdfd960c721c6ccca99f0f77f6774
Evgeny <transingularity@gmail.com>
2020-10-03 10:08:40 +0300

    refactor: Изменен скрипт миграции

--- hw09-jdbc/src/main/resources/db/migration/V1__initial_schema.sql
+++ hw09-jdbc/src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,3 +1,3 @@
 create table test(id int, name varchar(50));
-create table user(id long auto_increment, name varchar(50));
+create table user(id bigint(20) NOT NULL auto_increment, name varchar(255), age int(3));
 
