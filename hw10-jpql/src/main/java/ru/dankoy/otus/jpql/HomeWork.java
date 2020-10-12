487e0666afb09317c154221fe9174e093a60a07e
Evgeny <transingularity@gmail.com>
2020-10-12 18:01:30 +0300

    refactor: Добавлен todo комментарий

--- hw10-jpql/src/main/java/ru/dankoy/otus/jpql/HomeWork.java
+++ hw10-jpql/src/main/java/ru/dankoy/otus/jpql/HomeWork.java
@@ -19,6 +19,8 @@ import java.util.List;
 import java.util.Optional;
 
 
+//TODO: Сделать используя ConnectionPool
+
 public class HomeWork {
     private static final Logger logger = LoggerFactory.getLogger(HomeWork.class);
 
