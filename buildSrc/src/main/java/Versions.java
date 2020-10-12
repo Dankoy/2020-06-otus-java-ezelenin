7fb66e6c45fa9fe0ba73bb0faa02245fb8f067b4
Evgeny <transingularity@gmail.com>
2020-10-12 18:43:47 +0300

    refactor: Добавлены версии flyway, postgresql и testcontainers

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -8,4 +8,15 @@ public interface Versions {
     String asm = "8.0.1";
     String glassfishJson = "1.1.4";
     String protobuf = "3.12.1";
+    String gson = "2.8.6";
+
+    String flyway = "6.4.4";
+    String testcontainers = "1.13.0";
+    String postgresql = "42.2.5";
+
+    String jetty = "9.4.31.v20200723";
+    String freemarker = "2.3.30";
+    String sockjs = "1.1.2";
+    String stomp = "2.3.3-1";
+    String bootstrap = "4.4.1-1";
 }
