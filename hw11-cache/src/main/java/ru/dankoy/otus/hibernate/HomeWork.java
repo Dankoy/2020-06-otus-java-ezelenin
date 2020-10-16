de60214c9145c10c052db3e1c605a9577cc83aa2
Evgeny <transingularity@gmail.com>
2020-10-16 20:46:21 +0300

    refactor: Добавлены методы с используем кэшируемого hibernate

--- hw11-cache/src/main/java/ru/dankoy/otus/hibernate/HomeWork.java
+++ hw11-cache/src/main/java/ru/dankoy/otus/hibernate/HomeWork.java
@@ -3,20 +3,20 @@ package ru.dankoy.otus.hibernate;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.dankoy.otus.hibernate.cache.CustomCache;
-import ru.dankoy.otus.hibernate.cache.CustomCacheImpl;
 import ru.dankoy.otus.hibernate.core.dao.AddressDataSetDao;
 import ru.dankoy.otus.hibernate.core.dao.PhoneDataSetDao;
 import ru.dankoy.otus.hibernate.core.dao.UserDao;
 import ru.dankoy.otus.hibernate.core.model.AddressDataSet;
 import ru.dankoy.otus.hibernate.core.model.PhoneDataSet;
 import ru.dankoy.otus.hibernate.core.model.User;
+import ru.dankoy.otus.hibernate.hibernate.cacheddao.CachedUserDaoHibernate;
 import ru.dankoy.otus.hibernate.hibernate.dao.AddressDataSetDaoHibernate;
 import ru.dankoy.otus.hibernate.hibernate.dao.PhoneDataSetDaoHibernate;
 import ru.dankoy.otus.hibernate.hibernate.dao.UserDaoHibernate;
 import ru.dankoy.otus.hibernate.hibernate.sessionmanager.SessionManagerHibernate;
 import ru.dankoy.otus.hibernate.hibernate.utils.HibernateUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,31 +25,20 @@ public class HomeWork {
     private static final Logger logger = LoggerFactory.getLogger(HomeWork.class);
 
     private static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
+    private static UserDao userDaoWithCache;
 
     public static void main(String[] args) {
 
         SessionManagerHibernate sessionManagerHibernate = getSessionManager();
 
-        CustomCache<Integer, Integer> customCache = new CustomCacheImpl<>();
-
-        customCache.put(1,1);
-        logger.info("Add key: {}. toString: {}", 1, customCache.toString());
-        customCache.remove(1);
-        logger.info("Remove key: {}. toString: {}", 1, customCache.toString());
-        customCache.remove(1);
-        logger.info("Remove key: {}. toString: {}", 1, customCache.toString());
-
-
-
-
-//        var userId = insertUser(sessionManagerHibernate);
-//
-//        updateAddress(sessionManagerHibernate, userId);
-//        updatePhone(sessionManagerHibernate, userId);
-
+//        var userId = insertUserWithoutCache(sessionManagerHibernate);
+        var userId2 = insertUserWithCache(sessionManagerHibernate);
+        updateAddressWithCache(sessionManagerHibernate, userId2);
+        updatePhoneWithCache(sessionManagerHibernate, userId2);
 
     }
 
+// Все методы без кэшом
 
     /**
      * обавление нового телефона к юзеру
@@ -57,7 +46,7 @@ public class HomeWork {
      * @param sessionManagerHibernate
      * @param userId
      */
-    private static void updatePhone(SessionManagerHibernate sessionManagerHibernate, long userId) {
+    private static void updatePhoneWithoutCache(SessionManagerHibernate sessionManagerHibernate, long userId) {
 
         sessionManagerHibernate.beginSession();
 
@@ -89,7 +78,7 @@ public class HomeWork {
      * @param sessionManagerHibernate
      * @param userId
      */
-    private static void updateAddress(SessionManagerHibernate sessionManagerHibernate, long userId) {
+    private static void updateAddressWithoutCache(SessionManagerHibernate sessionManagerHibernate, long userId) {
 
         sessionManagerHibernate.beginSession();
 
@@ -113,11 +102,11 @@ public class HomeWork {
     }
 
     /**
-     * Запись юзера в базу и получение записанного юзера из базы
+     * Запись юзера в базу и получение записанного юзера из базы используя обычный Hibernate Dao
      *
      * @param sessionManagerHibernate
      */
-    private static long insertUser(SessionManagerHibernate sessionManagerHibernate) {
+    private static long insertUserWithoutCache(SessionManagerHibernate sessionManagerHibernate) {
 
         sessionManagerHibernate.beginSession();
 
@@ -148,6 +137,108 @@ public class HomeWork {
         return userId;
 
     }
+
+    // Все методы с кэшом
+
+    /**
+     * Запись юзера в базу и получение записанного юзера из базы используя Dao Hibernate с кастомным кэшем
+     *
+     * @param sessionManagerHibernate
+     */
+    private static long insertUserWithCache(SessionManagerHibernate sessionManagerHibernate) {
+
+        sessionManagerHibernate.beginSession();
+
+        UserDao userDao = new UserDaoHibernate(sessionManagerHibernate);
+        userDaoWithCache = new CachedUserDaoHibernate(userDao);
+
+        // Запись юзера в базу
+        List<PhoneDataSet> phoneDataSets = new ArrayList<>();
+        phoneDataSets.add(new PhoneDataSet("phone1"));
+        phoneDataSets.add(new PhoneDataSet("phone2"));
+        phoneDataSets.add(new PhoneDataSet("phone3"));
+        phoneDataSets.add(new PhoneDataSet("phone4"));
+
+        AddressDataSet addressDataSet = new AddressDataSet("nice address");
+        User newUser = new User("name", 12, addressDataSet, phoneDataSets);
+        addressDataSet.setUser(newUser);
+        phoneDataSets.forEach(phone -> phone.setUser(newUser));
+
+
+        var userId = userDaoWithCache.insertUser(newUser);
+
+        logger.info("-------------------------------- ");
+
+        // Получение юзера из базы
+        Optional<User> foundUser = userDaoWithCache.findById(userId);
+        foundUser.ifPresentOrElse(
+                crUser -> logger.info("Found user, name:{}", crUser.getName()),
+                () -> logger.info("user was not found")
+        );
+
+        sessionManagerHibernate.commitSession();
+
+        return userId;
+
+    }
+
+    /**
+     * обавление нового телефона к юзеру
+     *
+     * @param sessionManagerHibernate
+     * @param userId
+     */
+    private static void updatePhoneWithCache(SessionManagerHibernate sessionManagerHibernate, long userId) {
+
+        sessionManagerHibernate.beginSession();
+
+        // Получение юзера из базы
+        Optional<User> foundUser = userDaoWithCache.findById(userId);
+
+        if (foundUser.isPresent()) {
+            var phones = foundUser.get().getPhoneDataSets();
+
+            PhoneDataSet newPhone = new PhoneDataSet("new phone");
+            newPhone.setUser(foundUser.get());
+
+            phones.add(newPhone);
+
+            PhoneDataSetDao phoneDataSetDao = new PhoneDataSetDaoHibernate(sessionManagerHibernate);
+
+            phoneDataSetDao.insertPhone(newPhone);
+
+            sessionManagerHibernate.commitSession();
+        }
+
+    }
+
+    /**
+     * Обновление адреса у существующего юзера
+     *
+     * @param sessionManagerHibernate
+     * @param userId
+     */
+    private static void updateAddressWithCache(SessionManagerHibernate sessionManagerHibernate, long userId) {
+
+        sessionManagerHibernate.beginSession();
+
+        // Получение юзера из базы
+        Optional<User> foundUser = userDaoWithCache.findById(userId);
+
+        if (foundUser.isPresent()) {
+            var address = foundUser.get().getAddress();
+
+            address.setStreet("updated street");
+
+            AddressDataSetDao addressDataSetDao = new AddressDataSetDaoHibernate(sessionManagerHibernate);
+
+            addressDataSetDao.updateAddress(address);
+
+            sessionManagerHibernate.commitSession();
+        }
+
+    }
+
 
     /**
      * Получение менеджера сессий hibernate
