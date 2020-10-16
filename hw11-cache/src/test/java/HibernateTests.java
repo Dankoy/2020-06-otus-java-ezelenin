f4385554da5bfc2b78b4538308106f0253284b74
Evgeny <transingularity@gmail.com>
2020-10-16 21:49:55 +0300

    test: Добавлен тест с использованием DbService

--- hw11-cache/src/test/java/HibernateTests.java
+++ hw11-cache/src/test/java/HibernateTests.java
@@ -4,12 +4,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.dankoy.otus.hibernate.cache.CustomCache;
+import ru.dankoy.otus.hibernate.cache.CustomCacheImpl;
+import ru.dankoy.otus.hibernate.cache.CustomCacheListener;
+import ru.dankoy.otus.hibernate.cache.CustomCacheListenerImpl;
 import ru.dankoy.otus.hibernate.core.dao.AddressDataSetDao;
 import ru.dankoy.otus.hibernate.core.dao.PhoneDataSetDao;
 import ru.dankoy.otus.hibernate.core.dao.UserDao;
 import ru.dankoy.otus.hibernate.core.model.AddressDataSet;
 import ru.dankoy.otus.hibernate.core.model.PhoneDataSet;
 import ru.dankoy.otus.hibernate.core.model.User;
+import ru.dankoy.otus.hibernate.core.service.userservice.DBServiceUser;
+import ru.dankoy.otus.hibernate.core.service.userservice.DbServiceUserCacheImpl;
+import ru.dankoy.otus.hibernate.core.service.userservice.DbServiceUserImpl;
 import ru.dankoy.otus.hibernate.hibernate.cacheddao.CachedUserDaoHibernate;
 import ru.dankoy.otus.hibernate.hibernate.dao.AddressDataSetDaoHibernate;
 import ru.dankoy.otus.hibernate.hibernate.dao.PhoneDataSetDaoHibernate;
@@ -115,7 +122,74 @@ class HibernateTests {
 
         sessionManagerHibernate.commitSession();
 
+    }
+
+
+    @Test
+    @DisplayName("Проверка, что юзер добавился в базу и вернулся идентичным из базы c кэшем с использованием " +
+            "DbService")
+    void insertUserWithCacheDbService() {
+
+        CustomCache<Long, Optional<User>> cache = new CustomCacheImpl<>();
+        CustomCacheListener<Long, Optional<User>> listener = new CustomCacheListenerImpl<>();
+        cache.addListener(listener);
+
+        UserDao userDao = new UserDaoHibernate(sessionManagerHibernate);
+        DBServiceUser dbServiceUser = new DbServiceUserImpl(userDao);
+        DBServiceUser dbServiceUserCache = new DbServiceUserCacheImpl(dbServiceUser, cache);
+
+        // Запись юзера в базу
+        List<PhoneDataSet> phoneDataSets = List.of(new PhoneDataSet("phone1"), new PhoneDataSet("phone2"),
+                new PhoneDataSet("phone3"));
+        AddressDataSet addressDataSet = new AddressDataSet("nice address");
+        User newUser = new User("name", 12, addressDataSet, phoneDataSets);
+        addressDataSet.setUser(newUser);
+        phoneDataSets.forEach(phone -> phone.setUser(newUser));
+
+        var userId = dbServiceUserCache.saveUser(newUser);
+
+        logger.info("Getting user by Id, id:{}", userId);
+        Optional<User> foundUser = dbServiceUserCache.getUser(userId);
+        foundUser.ifPresentOrElse(
+                crUser -> logger.info("Found user, name:{}", crUser.getName()),
+                () -> logger.info("user was not found")
+        );
+
+        assertThat(foundUser.get())
+                .usingRecursiveComparison()
+                .isEqualTo(newUser);
 
     }
+
+    @Test
+    @DisplayName("Проверка, что юзер добавился в базу и вернулся идентичным из базы без кэша с использованием " +
+            "DbService")
+    void insertUserWithoutCacheDbService() {
+
+        UserDao userDao = new UserDaoHibernate(sessionManagerHibernate);
+        DBServiceUser dbServiceUser = new DbServiceUserImpl(userDao);
+
+        // Запись юзера в базу
+        List<PhoneDataSet> phoneDataSets = List.of(new PhoneDataSet("phone1"), new PhoneDataSet("phone2"),
+                new PhoneDataSet("phone3"));
+        AddressDataSet addressDataSet = new AddressDataSet("nice address");
+        User newUser = new User("name", 12, addressDataSet, phoneDataSets);
+        addressDataSet.setUser(newUser);
+        phoneDataSets.forEach(phone -> phone.setUser(newUser));
+
+        var userId = dbServiceUser.saveUser(newUser);
+
+        logger.info("Getting user by Id, id:{}", userId);
+        Optional<User> foundUser = dbServiceUser.getUser(userId);
+        foundUser.ifPresentOrElse(
+                crUser -> logger.info("Found user, name:{}", crUser.getName()),
+                () -> logger.info("user was not found")
+        );
+
+        assertThat(foundUser.get())
+                .usingRecursiveComparison()
+                .isEqualTo(newUser);
+    }
+
 
 }
