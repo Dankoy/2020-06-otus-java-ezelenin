84a78b06b968c43d32d36c59d56d11632f30db9e
Evgeny <transingularity@gmail.com>
2020-10-16 21:18:57 +0300

    refactor: Добавлена примеры с использованием кэша через DbService

--- hw11-cache/src/main/java/ru/dankoy/otus/hibernate/HomeWork.java
+++ hw11-cache/src/main/java/ru/dankoy/otus/hibernate/HomeWork.java
@@ -3,12 +3,19 @@ package ru.dankoy.otus.hibernate;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.dankoy.otus.hibernate.cache.CustomCache;
+import ru.dankoy.otus.hibernate.cache.CustomCacheImpl;
+import ru.dankoy.otus.hibernate.cache.CustomCacheListener;
+import ru.dankoy.otus.hibernate.cache.CustomCacheListenerImpl;
 import ru.dankoy.otus.hibernate.core.dao.AddressDataSetDao;
 import ru.dankoy.otus.hibernate.core.dao.PhoneDataSetDao;
 import ru.dankoy.otus.hibernate.core.dao.UserDao;
 import ru.dankoy.otus.hibernate.core.model.AddressDataSet;
 import ru.dankoy.otus.hibernate.core.model.PhoneDataSet;
 import ru.dankoy.otus.hibernate.core.model.User;
+import ru.dankoy.otus.hibernate.core.service.userservice.DBServiceUser;
+import ru.dankoy.otus.hibernate.core.service.userservice.DbServiceUserCacheImpl;
+import ru.dankoy.otus.hibernate.core.service.userservice.DbServiceUserImpl;
 import ru.dankoy.otus.hibernate.hibernate.cacheddao.CachedUserDaoHibernate;
 import ru.dankoy.otus.hibernate.hibernate.dao.AddressDataSetDaoHibernate;
 import ru.dankoy.otus.hibernate.hibernate.dao.PhoneDataSetDaoHibernate;
@@ -31,14 +38,45 @@ public class HomeWork {
 
         SessionManagerHibernate sessionManagerHibernate = getSessionManager();
 
+        // использование Hibernate с кэшем. Работает только внутри одной транзакции. Можно сохранить юзера и получить
+        // его. Обновления не работают так как при операции update вызывается метод persist на объект которого нет в
+        // кэше 1 уровня.
+
+        // Пример с использованием проксей ru.dankoy.otus.hibernate.hibernate.cacheddao.CachedUserDaoHibernate
 //        var userId = insertUserWithoutCache(sessionManagerHibernate);
-        var userId2 = insertUserWithCache(sessionManagerHibernate);
-        updateAddressWithCache(sessionManagerHibernate, userId2);
-        updatePhoneWithCache(sessionManagerHibernate, userId2);
+//        var userId2 = insertUserWithCache(sessionManagerHibernate);
+//        updateAddressWithCache(sessionManagerHibernate, userId2);
+//        updatePhoneWithCache(sessionManagerHibernate, userId2);
+
+
+        // Пример с DBService
+        CustomCache<Long, Optional<User>> cache = new CustomCacheImpl<>();
+        CustomCacheListener<Long, Optional<User>> listener = new CustomCacheListenerImpl<>();
+        cache.addListener(listener);
+
+        UserDao userDao = new UserDaoHibernate(sessionManagerHibernate);
+        DBServiceUser dbServiceUser = new DbServiceUserImpl(userDao);
+        DBServiceUser dbServiceUserCache = new DbServiceUserCacheImpl(dbServiceUser, cache);
+
+        // Запись юзера в базу
+        List<PhoneDataSet> phoneDataSets = List.of(new PhoneDataSet("phone1"), new PhoneDataSet("phone2"),
+                new PhoneDataSet("phone3"));
+        AddressDataSet addressDataSet = new AddressDataSet("nice address");
+        User newUser = new User("name", 12, addressDataSet, phoneDataSets);
+        addressDataSet.setUser(newUser);
+        phoneDataSets.forEach(phone -> phone.setUser(newUser));
+
+        var userId = dbServiceUserCache.saveUser(newUser);
+
+        Optional<User> foundUser = dbServiceUserCache.getUser(userId);
+        foundUser.ifPresentOrElse(
+                crUser -> logger.info("Found user, name:{}", crUser.getName()),
+                () -> logger.info("user was not found")
+        );
 
     }
 
-// Все методы без кэшом
+// Все методы без кэша
 
     /**
      * обавление нового телефона к юзеру
