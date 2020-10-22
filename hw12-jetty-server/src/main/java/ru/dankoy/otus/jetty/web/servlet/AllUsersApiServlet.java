fe5307df8135b94783aae26b98ee0aa7a9f4124b
Evgeny <transingularity@gmail.com>
2020-10-22 15:39:41 +0300

    refactor: Реализована вставка нового пользователя в бд

--- hw12-jetty-server/src/main/java/ru/dankoy/otus/jetty/web/servlet/AllUsersApiServlet.java
+++ hw12-jetty-server/src/main/java/ru/dankoy/otus/jetty/web/servlet/AllUsersApiServlet.java
@@ -2,6 +2,8 @@ package ru.dankoy.otus.jetty.web.servlet;
 
 import com.google.gson.Gson;
 import ru.dankoy.otus.jetty.core.dao.UserDao;
+import ru.dankoy.otus.jetty.core.model.AddressDataSet;
+import ru.dankoy.otus.jetty.core.model.PhoneDataSet;
 import ru.dankoy.otus.jetty.core.model.User;
 import ru.dankoy.otus.jetty.hibernate.sessionmanager.SessionManagerHibernate;
 
@@ -9,8 +11,11 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class AllUsersApiServlet extends HttpServlet {
 
@@ -36,6 +41,34 @@ public class AllUsersApiServlet extends HttpServlet {
         out.print(gson.toJson(user));
 
         sessionManagerHibernate.commitSession();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        sessionManagerHibernate.beginSession();
+
+        BufferedReader reader = request.getReader();
+        String body = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        User user = gson.fromJson(body, User.class);
+
+        //Заполнение связей между юзером, адресом и телефонами
+        AddressDataSet addressDataSet = user.getAddress();
+        addressDataSet.setUser(user);
+
+        List<PhoneDataSet> phoneDataSets = user.getPhoneDataSets();
+        phoneDataSets.forEach(phoneDataSet -> phoneDataSet.setUser(user));
+
+        var id = userDao.insertUser(user);
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        Optional<User> foundUser = userDao.findById(id);
+        response.setContentType("application/json;charset=UTF-8");
+        ServletOutputStream out = response.getOutputStream();
+        out.print(gson.toJson(foundUser.orElse(null)));
+
+        sessionManagerHibernate.commitSession();
+
     }
 
 }
