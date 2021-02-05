2555e005acd72b11c82fb14ef2b623ec8b0c2fa5
Evgeny <transingularity@gmail.com>
2021-02-05 19:22:39 +0300

    refactor: Изменено поле с UserDao на DBServiceUser

--- hw14-war-spring-mvc/src/main/java/ru/dankoy/otus/warmvc/web/controllers/UserRestController.java
+++ hw14-war-spring-mvc/src/main/java/ru/dankoy/otus/warmvc/web/controllers/UserRestController.java
@@ -2,23 +2,23 @@ package ru.dankoy.otus.warmvc.web.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.dankoy.otus.warmvc.core.dao.UserDao;
 import ru.dankoy.otus.warmvc.core.model.User;
+import ru.dankoy.otus.warmvc.core.service.userservice.DBServiceUser;
 
 import java.util.List;
 
 @RestController
 public class UserRestController {
 
-    private final UserDao userDao;
+    private final DBServiceUser dbServiceUser;
 
-    public UserRestController(UserDao userDao) {
-        this.userDao = userDao;
+    public UserRestController(DBServiceUser dbServiceUser) {
+        this.dbServiceUser = dbServiceUser;
     }
 
     @GetMapping({"/api/user"})
     public List<User> getClients() {
-        return userDao.getAllUsers();
+        return dbServiceUser.getAllUsers();
     }
 
 }
