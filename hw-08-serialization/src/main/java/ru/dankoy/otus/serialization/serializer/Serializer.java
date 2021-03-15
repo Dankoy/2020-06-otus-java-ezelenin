fea709a622dd83ef3b547704cdc0c23fe9825fd1
Evgeny <transingularity@gmail.com>
2021-03-15 18:54:20 +0300

    refactor: удалены лишние методы. добавлена работа с массивами

--- hw-08-serialization/src/main/java/ru/dankoy/otus/serialization/serializer/Serializer.java
+++ hw-08-serialization/src/main/java/ru/dankoy/otus/serialization/serializer/Serializer.java
@@ -1,12 +1,10 @@
 package ru.dankoy.otus.serialization.serializer;
 
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -16,21 +14,18 @@ import java.util.List;
 public class Serializer {
 
     private JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
-    private JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
 
     public String serializeObject(Object object) throws IOException {
 
-        if (object != null) {
-            buildJson(object);
+        if (object == null) {
+            return "null";
         } else {
-            return null;
+            return buildJson(object);
         }
 
-
-        return getJsonStringFromJsonObjectBuilder(objectBuilder);
     }
 
-    //TODO Не реализована рекурсия и поиск и проверка массивов
+    //TODO Не реализована рекурсия
 
     /**
      * Строительство json.
@@ -38,44 +33,164 @@ public class Serializer {
      * @param object
      * @throws IOException
      */
-    private void buildJson(Object object) {
+    private String buildJson(Object object) throws IOException {
+
+        if (isPrimitive(object)) {
+            return String.valueOf(object);
+        } else if (isCharOrString(object)) {
+            return "\"" + object + "\"";
+        } else if (object.getClass().isArray()) {
+            return String.valueOf(serializeArray(object));
+        } else if (object instanceof Collection) {
+            return String.valueOf(serializeCollection(object));
+        }
+
+        return buildObject(object);
+
+    }
+
+
+    /**
+     * Проверка на примитивный тип
+     *
+     * @param obj
+     * @return
+     */
+    private boolean isPrimitive(Object obj) {
+        if (obj instanceof Boolean) return true;
+        if (obj instanceof Byte) return true;
+        if (obj instanceof Integer) return true;
+        if (obj instanceof Short) return true;
+        if (obj instanceof Long) return true;
+        if (obj instanceof Float) return true;
+        if (obj instanceof Double) return true;
+        return false;
+    }
+
+    /**
+     * Проверка на String или Char
+     *
+     * @param obj
+     * @return
+     */
+    private boolean isCharOrString(Object obj) {
+
+        if (obj instanceof String) return true;
+        else return obj instanceof Character;
+    }
+
+
+    /**
+     * Билд json объекта
+     *
+     * @param object
+     * @return
+     * @throws IOException
+     */
+    private String buildObject(Object object) throws IOException {
 
         Class clazz = getClassFromObject(object);
         Field[] objectFields = getObjectFields(clazz);
 
         for (Field objectField : objectFields) {
-            Class<?> fieldType = getFieldType(objectField);
-            boolean isFieldPrimitive = isFieldPrimitive(fieldType);
-            if (isFieldPrimitive) {
 
-                switchCasePrimitiveTypeToObjectBuilder(fieldType, object, objectField);
+            Object fieldValue = getFieldValue(object, objectField);
 
-            } else {
+            if (fieldValue != null) {
 
-                switchCaseWrapperTypeToObjectBuilder(fieldType, object, objectField);
+                // парсинг примитива
+                if (isPrimitive(fieldValue) || isCharOrString(fieldValue)) {
+                    checkPrimitiveFieldTypeAndGetCorrectValue(object, objectField);
+                }
+
+                // парсинг коллекции
+                if (fieldValue instanceof Collection) {
+                    JsonValue array = serializeCollection(fieldValue);
+                    objectBuilder.add(getFieldName(objectField), array);
+
+                }
+
+                // Парсинг массива
+                if (fieldValue.getClass().isArray()) {
+                    JsonArray array = serializeArray(fieldValue);
+
+                    objectBuilder.add(getFieldName(objectField), array);
+                }
             }
 
-            if (checkIfInterfaceIsPresent(getInterfaces(fieldType), Collection.class)) {
+        }
 
-                List<?> list = null;
-                Object fieldValue = getFieldValue(object, objectField);
-                if (fieldValue != null) {
-                    list = new ArrayList<>((Collection<?>) getFieldValue(object, objectField));
-                } else {
-                    list = new ArrayList<>();
-                }
-                for (Object item : list) {
-                    switchCaseAnyTypeToArrayBuilder(item);
-                }
-                objectBuilder.add(getFieldName(objectField), arrayBuilder);
+        return getJsonStringFromJsonObjectBuilder(objectBuilder);
 
-            }
+    }
+
+    /**
+     * Сериализация массива
+     *
+     * @param obj
+     * @return
+     */
+    private JsonArray serializeArray(Object obj) {
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+
+        for (int i = 0; i < Array.getLength(obj); i++) {
+            switchSerializeArray(Array.get(obj, i), arrayBuilder);
+        }
+
+        return arrayBuilder.build();
+
+    }
+
+    /**
+     * Сериализация коллекции
+     *
+     * @param collection
+     * @return
+     */
+    private JsonArray serializeCollection(Object collection) {
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+
+        List<?> list = new ArrayList<>((Collection<?>) collection);
+        for (Object item : list) {
+            switchSerializeArray(item, arrayBuilder);
+        }
+
+        return arrayBuilder.build();
+
+    }
+
+
+    /**
+     * Проверка типа объекта в листе и приведение его к нужному типу
+     *
+     * @param value
+     */
+    private void switchSerializeArray(Object value, JsonArrayBuilder arrayBuilder) {
+
+        if (value instanceof Integer) {
+            arrayBuilder.add((int) value);
+        } else if (value instanceof Boolean) {
+            arrayBuilder.add((boolean) value);
+        } else if (value instanceof Byte) {
+            arrayBuilder.add((byte) value);
+        } else if (value instanceof Short) {
+            arrayBuilder.add((short) value);
+        } else if (value instanceof Long) {
+            arrayBuilder.add((long) value);
+        } else if (value instanceof Float) {
+            arrayBuilder.add((float) value);
+        } else if (value instanceof Double) {
+            arrayBuilder.add((double) value);
+        } else if (value instanceof String) {
+            arrayBuilder.add((String) value);
+        } else if (value instanceof Character) {
+            arrayBuilder.add((char) value);
         }
 
     }
 
     /**
-     * Получение финальной строки в формате json
+     * Получение финальной строки в формате json для объектов
      *
      * @param jsonObjectBuilder
      * @return
@@ -119,30 +234,6 @@ public class Serializer {
     }
 
     /**
-     * Получение типа поля
-     *
-     * @param field
-     * @return
-     */
-    private Class<?> getFieldType(Field field) {
-
-        return field.getType();
-
-    }
-
-    /**
-     * Получение типа поля
-     *
-     * @param object
-     * @return
-     */
-    private Class<?> getFieldType(Object object) {
-
-        return object.getClass();
-
-    }
-
-    /**
      * Получение значения поля у объекта
      *
      * @param object
@@ -169,255 +260,35 @@ public class Serializer {
     }
 
     /**
-     * Проверка, если тип примитивный или нет.
-     * По лекции 17 - считаем String тоже примитивным типом
-     *
-     * @param fieldTypeClass
-     * @return
-     */
-    private boolean isFieldPrimitive(Class<?> fieldTypeClass) {
-
-        if (fieldTypeClass.getSimpleName().equals("String"))
-            return true;
-
-        return fieldTypeClass.isPrimitive();
-    }
-
-    /**
-     * Получение списка интерфейсов, которые реализовывает класс типа поля
-     *
-     * @param field
-     * @return
-     */
-    private Class<?>[] getInterfaces(Class<?> field) {
-        return field.getInterfaces();
-    }
-
-    /**
-     * Проверяется, если у типа толя присутствует выбранный интерфейс.
-     * Например, проверяется интерфейс Collection, что бы определить, что поле имеет тип List.
-     *
-     * @param interfaces
-     * @param interfazeName
-     * @return
-     */
-    private boolean checkIfInterfaceIsPresent(Class<?>[] interfaces, Class<?> interfazeName) {
-        boolean result = false;
-        for (Class interfaze : interfaces) {
-            if (interfaze.equals(interfazeName)) {
-                result = true;
-            } else {
-                result = false;
-            }
-        }
-        return result;
-    }
-
-    /**
      * Конвертация значения переменной объекта и приведение ее к нужному примитивному типу.
      *
-     * @param fieldType
      * @param object
      * @param objectField
      */
-    private void switchCasePrimitiveTypeToObjectBuilder(Class<?> fieldType, Object object, Field objectField) {
+    private void checkPrimitiveFieldTypeAndGetCorrectValue(Object object, Field objectField) {
 
-        switch (fieldType.getSimpleName()) {
-            case ("int"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (int) getFieldValue(object, objectField));
-                break;
-            }
-            case ("boolean"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (boolean) getFieldValue(object, objectField));
-                break;
-            }
-            case ("String"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (String) getFieldValue(object, objectField));
-                break;
-            }
-            case ("long"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (long) getFieldValue(object, objectField));
-                break;
-            }
-            case ("double"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (double) getFieldValue(object, objectField));
-                break;
-            }
-            case ("float"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (float) getFieldValue(object, objectField));
-                break;
-            }
-            case ("byte"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (byte) getFieldValue(object, objectField));
-                break;
-            }
-            case ("char"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (char) getFieldValue(object, objectField));
-                break;
-            }
-            case ("short"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (short) getFieldValue(object, objectField));
-                break;
-            }
+        String fieldName = getFieldName(objectField);
+        Object fieldValue = getFieldValue(object, objectField);
+
+        if (fieldValue instanceof Integer) {
+            objectBuilder.add(fieldName, (int) fieldValue);
+        } else if (fieldValue instanceof Boolean) {
+            objectBuilder.add(fieldName, (boolean) fieldValue);
+        } else if (fieldValue instanceof Byte) {
+            objectBuilder.add(fieldName, (byte) fieldValue);
+        } else if (fieldValue instanceof Short) {
+            objectBuilder.add(fieldName, (short) fieldValue);
+        } else if (fieldValue instanceof Long) {
+            objectBuilder.add(fieldName, (long) fieldValue);
+        } else if (fieldValue instanceof Float) {
+            objectBuilder.add(fieldName, (float) fieldValue);
+        } else if (fieldValue instanceof Double) {
+            objectBuilder.add(fieldName, (double) fieldValue);
+        } else if (fieldValue instanceof String) {
+            objectBuilder.add(fieldName, (String) fieldValue);
+        } else if (fieldValue instanceof Character) {
+            objectBuilder.add(fieldName, (char) fieldValue);
         }
-    }
-
-    /**
-     * Конвертация значения переменной объекта и приведение ее к нужному типу обертки.
-     *
-     * @param fieldType
-     * @param object
-     * @param objectField
-     */
-    private void switchCaseWrapperTypeToObjectBuilder(Class<?> fieldType, Object object, Field objectField) {
-
-        switch (fieldType.getSimpleName()) {
-            case ("Integer"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (Integer) getFieldValue(object, objectField));
-                break;
-            }
-            case ("Boolean"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (Boolean) getFieldValue(object, objectField));
-                break;
-            }
-            case ("String"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (String) getFieldValue(object, objectField));
-                break;
-            }
-            case ("Long"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (Long) getFieldValue(object, objectField));
-                break;
-            }
-            case ("Double"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (Double) getFieldValue(object, objectField));
-                break;
-            }
-            case ("Float"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (Float) getFieldValue(object, objectField));
-                break;
-            }
-            case ("Byte"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (Byte) getFieldValue(object, objectField));
-                break;
-            }
-            case ("Char"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (Character) getFieldValue(object, objectField));
-                break;
-            }
-            case ("Short"): {
-                objectBuilder.add(getFieldName(objectField),
-                        (Short) getFieldValue(object, objectField));
-                break;
-            }
-        }
-
-    }
-
-    /**
-     * Проверка типа объекта в листе и приведение его к нужному типу
-     *
-     * @param value
-     */
-    private void switchCaseAnyTypeToArrayBuilder(Object value) {
-
-        if (!value.getClass().isPrimitive()) {
-
-            switch (value.getClass().getSimpleName()) {
-                case ("Integer"): {
-                    arrayBuilder.add((Integer) value);
-                    break;
-                }
-                case ("Boolean"): {
-                    arrayBuilder.add((Boolean) value);
-                    break;
-                }
-                case ("String"): {
-                    arrayBuilder.add((String) value);
-                    ;
-                    break;
-                }
-                case ("Long"): {
-                    arrayBuilder.add((Long) value);
-                    break;
-                }
-                case ("Double"): {
-                    arrayBuilder.add((Double) value);
-                    break;
-                }
-                case ("Float"): {
-                    arrayBuilder.add((Float) value);
-                    break;
-                }
-                case ("Byte"): {
-                    arrayBuilder.add((Byte) value);
-                    break;
-                }
-                case ("Char"): {
-                    arrayBuilder.add((Character) value);
-                    break;
-                }
-                case ("Short"): {
-                    arrayBuilder.add((Short) value);
-                    break;
-                }
-            }
-
-        } else {
-
-            switch (value.getClass().getSimpleName()) {
-                case ("int"): {
-                    arrayBuilder.add((int) value);
-                    break;
-                }
-                case ("boolean"): {
-                    arrayBuilder.add((boolean) value);
-                    break;
-                }
-                case ("long"): {
-                    arrayBuilder.add((long) value);
-                    break;
-                }
-                case ("double"): {
-                    arrayBuilder.add((double) value);
-                    break;
-                }
-                case ("float"): {
-                    arrayBuilder.add((float) value);
-                    break;
-                }
-                case ("byte"): {
-                    arrayBuilder.add((byte) value);
-                    break;
-                }
-                case ("char"): {
-                    arrayBuilder.add((char) value);
-                    break;
-                }
-                case ("short"): {
-                    arrayBuilder.add((short) value);
-                    break;
-                }
-            }
-
-        }
-
     }
 
 }
