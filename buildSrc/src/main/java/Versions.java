404d4633b8e3320c301c80ea8b7971a785df1e53
Evgeny <transingularity@gmail.com>
2021-03-15 18:55:09 +0300

    refactor: добавлены версии постгреса

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -8,4 +8,6 @@ public interface Versions {
     String asm = "8.0.1";
     String glassfishJson = "1.1.4";
     String protobuf = "3.12.1";
+    String testcontainers = "1.13.0";
+    String postgresql = "42.2.5";
 }
