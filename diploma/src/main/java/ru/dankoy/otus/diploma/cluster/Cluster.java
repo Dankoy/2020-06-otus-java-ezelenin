7d2a0f6e77e60264f28534c178be961871178881
Evgeny <transingularity@gmail.com>
2021-04-10 14:25:53 +0300

    refactor: игнорируются поля суммы longitude и latitude

--- diploma/src/main/java/ru/dankoy/otus/diploma/cluster/Cluster.java
+++ diploma/src/main/java/ru/dankoy/otus/diploma/cluster/Cluster.java
@@ -1,5 +1,6 @@
 package ru.dankoy.otus.diploma.cluster;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import ru.dankoy.otus.diploma.core.model.Crash;
 
 import java.io.Serializable;
@@ -16,8 +17,10 @@ public interface Cluster extends Serializable {
 
     void renewCoordinates(double latitude, double longitude);
 
+    @JsonIgnore
     double getSumLatitude();
 
+    @JsonIgnore
     double getSumLongitude();
 
     long getAmountOfPoints();
