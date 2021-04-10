9b6712c32bd12fad7b2be39a0ed70696e5aad803
Evgeny <transingularity@gmail.com>
2021-04-10 14:26:29 +0300

    fix: добавлена ручка для получения класетеров с указанным размером

--- diploma/src/main/java/ru/dankoy/otus/diploma/web/controllers/KMeansClusterRestController.java
+++ diploma/src/main/java/ru/dankoy/otus/diploma/web/controllers/KMeansClusterRestController.java
@@ -1,6 +1,7 @@
 package ru.dankoy.otus.diploma.web.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.dankoy.otus.diploma.cluster.Cluster;
 import ru.dankoy.otus.diploma.clusteralgorithms.kmeans.KMeansImpl;
@@ -34,6 +35,16 @@ public class KMeansClusterRestController {
         KMeansImpl kMeans = new KMeansImpl();
 
         return kMeans.cluster(crashes, 15);
+    }
+
+    @GetMapping(value = "/cluster/kmeans/nonmotorist/{clusterSize}")
+    public List<Cluster> getClustersForNonMotoristsByClusterSize(
+            @PathVariable(name = "clusterSize") int clusterSize) throws Exception {
+        List<Crash> crashes = dbServiceCrash.getCrashesWithNonMotorists();
+
+        KMeansImpl kMeans = new KMeansImpl();
+
+        return kMeans.cluster(crashes, clusterSize);
     }
 
 }
